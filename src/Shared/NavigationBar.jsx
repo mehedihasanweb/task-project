b98ff12b50cf389a5d5d175fc3e3c3c7a93b4032
mehedi hasan <mdmehedihasananik111@gmail.com>
2023-06-07 20:36:43 +0600

    update some part

--- src/Shared/NavigationBar.jsx
+++ src/Shared/NavigationBar.jsx
@@ -18,7 +18,7 @@ const NavigationBar = () => {
                         style={{ maxHeight: '100px' }}
                         navbarScroll
                     >
-                        <Nav.Link href="#action1">Home</Nav.Link>
+                        <Nav.Link href="#action1">Movies</Nav.Link>
                         <Nav.Link href="#action2">Link</Nav.Link>
                         <Nav.Link href="#" disabled>
                             Link
